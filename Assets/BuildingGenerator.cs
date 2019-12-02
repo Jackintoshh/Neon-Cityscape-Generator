5045cb86ae0cbcf6e1a7bf8c5ef262e1212aad26
Jackintoshh <jjwright10@hotmail.com>
2019-12-02 19:03:02 +0000

    Added basic street + basic building generator

---
+++ Assets/BuildingGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float height, width, botsize, midsize, topsize;
+    public int numPieces, numBuildings;
+    public GameObject street, bottom, middle, top;
+    //public List<GameObject> middle = new List<GameObject>();
+    //public List<GameObject> bottom = new List<GameObject>();
+    Vector3 loc = new Vector3();
+    //Vector3 midpos = new Vector3();
+    public string spawnNo, BuildingSpawn;
+    void Start()
+    {
+        Instantiate(street, gameObject.transform);
+        numPieces = 2;
+        numBuildings = 4;
+       // for (int i = 0; i < numBuildings; i++)
+        //{
+            //spawnNo = i.ToString();
+            //BuildingSpawn = "BuildingSpawn" + spawnNo;
+            loc = street.transform.Find("BuildingSpawn3").position;
+            Debug.Log(spawnNo);
+            Debug.Log(BuildingSpawn);
+            loc += new Vector3(0, 0.5f, 0);
+            bottom = (GameObject.CreatePrimitive(PrimitiveType.Cube));
+            bottom.transform.position = loc;
+            botsize = Random.Range(1.5f, 3.5f);
+            bottom.transform.localScale = new Vector3(botsize, Random.Range(1.5f, 2.5f), botsize);
+            //loc += new Vector3(0, 1.5f, 0);
+            Debug.Log(loc);
+            for (int j = 0; j < numPieces; j++)
+            {
+                loc += new Vector3(0, 1.5f, 0);
+                midsize = Random.Range(1.0f, 3.0f);
+                middle = (GameObject.CreatePrimitive(PrimitiveType.Cube));
+                middle.transform.position = loc;
+                middle.transform.localScale = new Vector3(midsize, Random.Range(1.5f, 2.5f), midsize);
+                
+            }
+
+        
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
