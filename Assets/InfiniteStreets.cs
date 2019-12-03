0deb273c2c11bffaea837b38746a32d1593bd280
Jackintoshh <jjwright10@hotmail.com>
2019-12-03 19:58:19 +0000

    Generate world with tiles

--- Assets/InfiniteStreets.cs
+++ Assets/InfiniteStreets.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class InfiniteStreets : MonoBehaviour
 {
     public GameObject street;
+    int planeSize = 10;
+    int halfTilesX = 10;
+    int halfTilesY = 10;
+    Vector3 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(street, gameObject.transform);
+        startPos = Vector3.zero;
+
+        for(int x = 0; x < halfTilesX; x++)
+        {
+            for(int z = 0; z < halfTilesY; z++)
+            {
+                Vector3 pos = new Vector3((x * planeSize + startPos.x), 0, (z * planeSize + startPos.z));
+                GameObject t = (GameObject)Instantiate(street, pos, Quaternion.identity);
+            }
+        }
+        
+
     }
 
     // Update is called once per frame
