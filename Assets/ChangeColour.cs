81f1b3d6d0f72f5f24e1faffc173e29e190a9fd5
Jackintoshh <jjwright10@hotmail.com>
2019-12-18 02:44:59 +0000

    Basic pulsing lights

---
+++ Assets/ChangeColour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColour : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public Color startColour, endColour;
+    float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = (Time.time - startTime) * speed;
+        Debug.Log(GetComponent<Light>().color);
+        GetComponent<Light>().color = Color.Lerp(startColour, endColour, t);
+        //GetComponent<Light>().intensity = 20;
+        //Flash();
+        if(GetComponent<Light>().color == endColour)
+        {
+            GetComponent<Light>().color = startColour;
+            startTime = Time.time;
+        }
+        //GetComponent<Light>().color = Color.Lerp(startColour, endColour, t);
+    }
+
+    void Flash()
+    {
+        GetComponent<Light>().intensity = Mathf.MoveTowards(0, 20, 20);
+    }
+}
